676165b7b8568d8fc88020cb8dd5c136011fa811
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-04-14 15:11:29 +0700

    Membuat perulangan do while yang di dalamnya untuk memasukkan element yang dicari yang akan dimasukkan ke variabel item, lalu perulangan comparisons, kemudian perbandingan nilai array serta untuk perulangan program dengan variabel ch.

--- Linearsearch/Linearsearch.cpp
+++ Linearsearch/Linearsearch.cpp
@@ -34,3 +34,30 @@ void LinearSearch()
 {
 	char ch; 
 	int comparisons; //Number of comparisons
+
+	do
+	{
+		//Accept the number to be searched
+		cout << "\nEnter the element you want to search: "; //Langkah 1
+		int item;
+		cin >> item;
+
+		comparisons = 0;
+		for (i = 0; i < n; i++) //Langkah 2, 3 dan 4
+		{
+			comparisons++;
+			if (arr[i] == item) //Langkah 5A found
+			{
+				cout << "\n" << item << "found at position" << (i + 1) << endl;
+				break;
+			}
+		}
+
+		if (i == n) //Langkah 58 not found
+			cout << "\n" << item << "not found in the array\n";
+		cout << "\nNumber of comparisons: " << comparisons << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'Y'));
+}
